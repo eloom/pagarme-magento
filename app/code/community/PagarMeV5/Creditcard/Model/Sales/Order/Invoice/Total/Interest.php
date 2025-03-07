cceabadac1b734aeae584961a9838066771d3588
élOOm <contato@eloom.com.br>
2025-03-07 20:50:25 -0300

    Update Interest.php

--- app/code/community/PagarMeV5/Creditcard/Model/Sales/Order/Invoice/Total/Interest.php
+++ app/code/community/PagarMeV5/Creditcard/Model/Sales/Order/Invoice/Total/Interest.php
@@ -1,99 +1,21 @@
 <?php
 
-use PagarMeV5_Creditcard_Model_Installments as Installments;
-
 class PagarMeV5_Creditcard_Model_Sales_Order_Invoice_Total_Interest extends Mage_Sales_Model_Order_Invoice_Total_Abstract {
 
-	use PagarMeV5_Core_Trait_ConfigurationsAccessor;
+	protected $_code = 'pagarmev5_interest';
 
-	/**
-	 * @var Mage_Sales_Model_Order
-	 */
-	protected $order;
-
-	/**
-	 * @param Mage_Sales_Model_Order_Invoice $invoice
-	 *
-	 * @return PagarMeV5_Creditcard_Model_InvoiceTotals
-	 */
 	public function collect(Mage_Sales_Model_Order_Invoice $invoice) {
-		$this->order = $invoice->getOrder();
+		parent::collect($invoice);
+		$order = $invoice->getOrder();
+		$baseTotalInterestAmount = $order->getPagarmev5BaseInterestAmount();
+		$totalInterestAmount = Mage::app()->getStore()->convertPrice($baseTotalInterestAmount);
 
-		$transaction = \Mage::getModel('pagarmev5_core/service_order')
-			->getTransactionByOrderId(
-				$this->order->getId()
-			);
+		$invoice->setPagarmev5InterestAmount($totalInterestAmount);
+		$invoice->setPagarmev5BaseInterestAmount($baseTotalInterestAmount);
 
-		if ($this->shouldUpdateRateAmount($transaction)) {
-			$transaction->setRateAmount(
-				$this->updateRateAmount($transaction, $invoice)
-			);
-		}
-
-		$invoice->setGrandTotal(
-			$invoice->getGrandTotal() + $transaction->getRateAmount()
-		);
-		$invoice->setBaseGrandTotal(
-			$invoice->getBaseGrandTotal() + $transaction->getRateAmount()
-		);
+		$invoice->setGrandTotal($invoice->getGrandTotal() + $invoice->getPagarmev5InterestAmount());
+		$invoice->setBaseGrandTotal($invoice->getBaseGrandTotal() + $invoice->getPagarmev5BaseInterestAmount());
 
 		return $this;
-	}
-
-	/**
-	 * @param PagarMeV5_Core_Model_Transaction $transaction
-	 * @param Mage_Sales_Model_Order_Invoice $invoice
-	 *
-	 * @return float
-	 */
-	private function updateRateAmount(
-		PagarMeV5_Core_Model_Transaction $transaction,
-		Mage_Sales_Model_Order_Invoice $invoice
-	) {
-		$sdk = Mage::getModel('pagarmev5_core/sdk_adapter')
-			->getSdk();
-
-		$orderTotal =
-			$invoice->getGrandTotal() + $this->order->getShippingAmount();
-
-		$installments = new Installments(
-			Mage::helper('pagarmev5_core')->parseAmountToCents($orderTotal),
-			$transaction->installments,
-			$this->getFreeInstallmentStoreConfig(),
-			$transaction->interest_rate,
-			$this->getMaxInstallmentStoreConfig(),
-			$sdk
-		);
-
-		$updatedRateAmount = Mage::helper('pagarmev5_core')
-			->parseAmountToCurrency($installments->getRateAmount());
-
-		$writeConnection = Mage::getSingleton('core/resource')
-			->getConnection('core_write');
-
-		$updateRateAmountQuery = sprintf(
-			'UPDATE pagarmev5_transaction SET %s',
-			'rate_amount = :rateAmount WHERE order_id = :orderId;'
-		);
-
-		$queryValues = [
-			'rateAmount' => $updatedRateAmount,
-			'orderId' => $this->order->getId()
-		];
-
-		$writeConnection->query($updateRateAmountQuery, $queryValues);
-
-		return $updatedRateAmount;
-	}
-
-	/**
-	 * @param PagarMeV5_Core_Model_Transaction $transaction
-	 *
-	 * @return bool
-	 */
-	private function shouldUpdateRateAmount(
-		PagarMeV5_Core_Model_Transaction $transaction
-	) {
-		return (bool)$transaction->getInterestRate();
 	}
 }
